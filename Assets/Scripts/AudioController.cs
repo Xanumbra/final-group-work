c3aa839e52f4bdfec5b09491a8dd903007db4b19
Ertuğ Obalar <eobalar@hotmail.com>
2020-12-06 17:37:29 +0100

    Added Victoria's code to the project
    In order to avoid conflict, I editted some of the parts so it is complatible with the project.

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class AudioController : MonoBehaviour
 {
     public static AudioController instance = null;
+
+    public AudioSource audioSource;
+
+    public float volume = 0.5f;
+    public List<AudioClip> clipArray = new List<AudioClip>();
+
     private void Awake()
     {
         if (instance == null)
@@ -16,6 +22,30 @@ public class AudioController : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void playFlipCardSound()
+    {
+        audioSource.PlayOneShot(clipArray[0], volume);
+    }
+    public void playMatchCardSound()
+    {
+        audioSource.PlayOneShot(clipArray[1], volume);
+    }
+
+    public void playMismatchCardSound()
+    {
+        audioSource.PlayOneShot(clipArray[2], volume);
+    }
+
+    public void playOnButtonPressSound()
+    {
+        audioSource.PlayOneShot(clipArray[3], volume);
+    }
+
 
 
 }
