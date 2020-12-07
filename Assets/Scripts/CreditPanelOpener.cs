2595304558248d8e0100a973eb31cf341823f7d2
KajaTml <71136461+KajaTml@users.noreply.github.com>
2020-12-07 08:42:09 +0200

    small cahnges in level canvas and working on main menu
    Changes I created:
    
    In every level i set canvas scaler screen match mode to Shrink (as this should check that everything fits to screen what ever resolution we use)
    In every level i changed the background image (to white table)
    
    In Main menu
    I rescaled the buttons smaller and changed the position of volume slider.
    I added new button Credits and created credits panel which will be opened/closed on click. For that I also added script CreditPanelOpener.
    
    I started working on Rules menus but need to continue.

---
+++ Assets/Scripts/CreditPanelOpener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditPanelOpener : MonoBehaviour
+{
+    // I will asign what is the game object I'm refering in this script
+    public GameObject CreditsPanel;
+
+    // I create new public method to open Credits panel.
+    public void OpenCreditsPanel()
+    {
+        // Here I'll check if this panel is opened or not
+        if (CreditsPanel != null)
+        {
+            // I'll ask if Credit's Panel is active 
+            bool isActive = CreditsPanel.activeSelf;
+
+            //I will toggle active state if activeSelf-method is called
+            CreditsPanel.SetActive(!isActive);
+        }
+        
+    }
+
+   
+}
